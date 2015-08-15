44d033b8bd300d9dfbf493bbbd4d0e34ad387f39
Barry1990 <barryfake21@gmail.com>
2015-08-15 13:18:36 +0200

    added missing EntityDeathEvent

---
+++ src/me/barry1990/skygrid/eventlistener/SkyGridOnEntityDeathEvent.java
@@ -0,0 +1,35 @@
+package me.barry1990.skygrid.eventlistener;
+
+import me.barry1990.skygrid.achievement.SkyGridAchievementManager;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+
+
+public class SkyGridOnEntityDeathEvent implements Listener {
+	
+	@EventHandler
+	public void OnEntityDeathEvent(EntityDeathEvent e) {
+		
+		/////////////////////////////////////
+		// NETHER CLEANER - Achievement
+		/////////////////////////////////////
+		
+		if (e.getEntity().getKiller() != null) {
+			
+			switch (e.getEntityType()) {
+				case PIG_ZOMBIE:
+				case GHAST:
+				case BLAZE: {
+					SkyGridAchievementManager.addNetherCleanerProgress(e.getEntity().getKiller());
+					break;
+				}
+				default:
+					break;
+			}
+			
+		}
+	}
+
+}
